d4846f30d0441fe9a6b8a08319c1d0dfd896cbf2
Habiba Khan <122211697+Habiba-K@users.noreply.github.com>
2025-04-13 09:56:04 +0500

    growth.py

--- growth.py
+++ growth.py
@@ -44,7 +44,7 @@ if uploaded_files:
 
             with col1:
                 if st.button(f"Remove duplicates from the files : {file.name}"):
-                    df.rop_duplicates(inplace=True)
+                    df.drop_duplicates(inplace=True)
                     st.write("✅ Duplicates removed!")
 
             with col2:
@@ -64,16 +64,16 @@ if uploaded_files:
 
         #Conversion Options
         st.header('Conversion Options')
-        conversion_type = st.radio(f"Convert ({file.name} to: " , ["CSV", "Excel"], key =file.name )
+        conversion_type = st.radio(f"Convert ({file.name} to:" , ["CSV", "Excel"], key =file.name )
         if st.button(f"Convert{file.name}"):
             buffer = BytesIO()
             if conversion_type =="CSV":
-                df.to.csv(buffer, index=False)
+                df.to_csv(buffer, index=False)
                 file_name =file.name.replace(file_ext,".csv")
                 mine_type ="text/csv"
 
             elif conversion_type =="Excel":
-                df.to.to_excel(buffer,index=False)
+                df.to_excel(buffer,index=False)
                 file_name = file.name.replace(file_ext, ".xlsx")
                 mine_type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             buffer.seek(0)
